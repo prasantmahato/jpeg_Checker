34204969a61e0ff606ac2d46680140c9b581b806
prasantmahato <prasantmahato33@gmail.com>
2020-05-17 11:21:56 +0530

    Added max size

--- sample.cpp
+++ sample.cpp
@@ -12,11 +12,15 @@ int main()
 	while(test--)
 	{
 		register float num;
-		register int i;
-		num=i=0;
+		register int i,size;
+		num=i=size=0;
+
 		cout<<"\nEnter the number for it's multiplication table:- ";
 		cin>>num;
-		for(i=1; i<=10; i++)
+		cout<<"\nEnter the max num, till you want ot multiply(max. 50):- ";
+		cin>>size;
+
+		for(i=1; i<=size; i++)
 			cout<<num<<" * "<<i<<": "<<setprecision(2)<<float(num*i)<<endl;
 		cout<<endl;		
 	}
