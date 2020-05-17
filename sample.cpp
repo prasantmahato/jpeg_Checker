a7858d9c22459a4f988ce8a87ffbc195f6d8234f
prasantmahato <prasantmahato33@gmail.com>
2020-05-17 11:06:17 +0530

    Changed program to generate Multiplication Table

--- sample.cpp
+++ sample.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-	cout<<"\nHello World"<<endl;
-	cout<<"Just Changing the content.."<<endl;
+	
+	int test=1;
+	cout<<"\nEnter Test Cases:- ";
+	cin>>test;
+
+	while(test--)
+	{
+		register float num;
+		register int i;
+		num=i=0;
+		cout<<"\nEnter the number for it's multiplication table:- ";
+		cin>>num;
+		for(i=1; i<=10; i++)
+			cout<<num<<" * "<<i<<": "<<setprecision(2)<<float(num*i)<<endl;
+		cout<<endl;		
+	}
 	return 0;
 }
