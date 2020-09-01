d713d8c50a587d44ee3912ce48016ad1a26bf49f
prasantmahato <prasantmahato33@gmail.com>
2020-09-01 12:34:24 +0530

    Adding direct executable file

--- jpegCheck.cpp
+++ jpegCheck.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <stdio.h>
 
@@ -9,7 +10,7 @@ int main(int argc, char *argv[])
         return 1;
     
     FILE *file = fopen(argv[1], "r");
-    
+
     if(file == NULL)
         return 1;
     
@@ -18,9 +19,9 @@ int main(int argc, char *argv[])
 
     //Check if bytes are 0xff 0xd8 0xff
     if(bytes[0]== 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff)
-        cout<<"MAybe\n";
+        cout<<"It is an JPEG FILE\n";
     else
-        cout<<"No\n";
+        cout<<"It is NOT AN JPEG FILE\n";
     return 0;
     
 }
